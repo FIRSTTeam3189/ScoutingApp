d63a63d73c9a7da483c648660fb25243d7f60c56
supermeap123 <garrentvanmeter@gmail.com>
2016-01-17 14:31:14 -0800

    Added more stuff

---
+++ ScoutingApp/Data/EventGrouping.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ScoutingModels.Data;
+using ScoutingModels.Test;
+
+namespace ScoutingApp.Data
+{
+    public class EventGrouping
+    {
+        public int Week { get; set; }
+
+        public string GroupName
+        {
+            get
+            {
+                if (Week == 0)
+                {
+                    return "Pre-Season";
+                }
+                return $"Week {Week}";  
+            }
+        }
+
+        public string ShortName
+        {
+            get
+            {
+                if (Week == 0)
+                {
+                    return "P";
+                }
+                return $"W{Week}";
+            }
+        }
+
+        public ICollection<Event> Events { get; set; }
+
+        public EventGrouping(int week, ICollection<Event> events)
+        {
+            events.IsNotNull();
+            Events = events;
+            Week = week;
+        }
+    }
+}
